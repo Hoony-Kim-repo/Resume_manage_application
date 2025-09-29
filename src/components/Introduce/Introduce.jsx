dbcd096b8c0afda510516e469f056ab93052d6e6
Hoony-Kim-repo <rlgnsrla512@gmail.com>
2025-09-29 15:34:42 -0400

    Introduce with 2 boxes

--- src/components/Introduce/Introduce.jsx
+++ src/components/Introduce/Introduce.jsx
@@ -1,8 +1,27 @@
+import { Box, Container } from "@chakra-ui/react";
+
+import styles from "./Introduce.module.css";
+
 const Introduce = () => {
   return (
-    <>
-      <h1>I am great software engineer!</h1>
-    </>
+    <div className={styles.container}>
+      <div className={styles.innerConainer}>
+        <Container maxW="xs" px="2">
+          <Box shadow="md" borderRadius="md" borderWidth="2px" padding="20px">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            consectetur, tortor in lacinia eleifend, dui nisl tristique nunc.
+          </Box>
+        </Container>
+      </div>
+      <div id={styles.innerConainer}>
+        <Container maxW="xs" px="2">
+          <Box shadow="md" borderRadius="md" borderWidth="2px" padding="20px">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            consectetur, tortor in lacinia eleifend, dui nisl tristique nunc.
+          </Box>
+        </Container>
+      </div>
+    </div>
   );
 };
 
