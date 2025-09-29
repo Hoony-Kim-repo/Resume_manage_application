4d45fe6f89dc015670ae52b5c12a014f29d455eb
Hoony-Kim-repo <rlgnsrla512@gmail.com>
2025-09-29 12:32:41 -0400

    Initial Intro Intorudce

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Introduce from "./components/Introduce/Introduce";
 import MyResume from "./Page/MyResume";
 
 function App() {
-  return <MyResume />;
+  return (
+    <>
+      <MyResume />
+      <Introduce />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Introduce/Introduce.jsx
@@ -0,0 +1,9 @@
+const Introduce = () => {
+  return (
+    <>
+      <h1>I am great software engineer!</h1>
+    </>
+  );
+};
+
+export default Introduce;
