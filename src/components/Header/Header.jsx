5620358434393c8a7b39c26a1c57dd3080a5ece9
Hoony-Kim-repo <rlgnsrla512@gmail.com>
2025-10-03 00:16:21 -0400

    Header to match to 100% of height

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,22 @@
-import { Avatar } from "@chakra-ui/react";
-import profileImg from "../../assets/my-profile-picture.jpg";
+import { Avatar, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import headerBackground from "../../assets/header_bg.webp";
+import profileImg from "../../assets/my-profile-picture.jpg";
 import styles from "./Header.module.css";
 
 const Header = ({ name, job }) => {
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => setHeight(window.innerHeight);
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
-      <div className={styles.container}>
+      <div className={styles.container} style={{ height: height }}>
         <img
           className={styles.image}
           src={headerBackground}
@@ -14,19 +24,35 @@ const Header = ({ name, job }) => {
           style={{ opacity: 0.3 }}
         />
 
-        <div className={styles.infoContainer}>
-          <Avatar.Root size="2xl" boxSize="200px">
-            <Avatar.Fallback name="Gihoon Kim" />
-            <Avatar.Image src={profileImg} />
-          </Avatar.Root>
-          <div className={styles.text}>
-            <h1>{name}</h1>
-            <h3>{job}</h3>
+        <div className={styles.wrapper}>
+          <div className={styles.infoContainer}>
+            <Avatar.Root size="2xl" boxSize="200px">
+              <Avatar.Fallback name="Gihoon Kim" />
+              <Avatar.Image src={profileImg} />
+            </Avatar.Root>
+            <div className={styles.text}>
+              <h1>{name}</h1>
+              <h3>{job}</h3>
+            </div>
+          </div>
+
+          <div className={styles.introduce}>
+            <Text fontWeight="bold" textStyle="xl">
+              Software engineer with a product mindset, known for shipping
+              reliable systems and elevating team standards.
+            </Text>
+            <Text fontWeight="bold" textStyle="xl">
+              From system design to deployment and observability, I deliver
+              maintainable solutions and communicate clearly with stakeholders.
+            </Text>
+            <Text fontWeight="bold" textStyle="xl">
+              I’m energized by tough problems and teams that value speed with
+              quality.
+            </Text>
+            <p></p>
           </div>
         </div>
       </div>
-
-      <p></p>
     </>
   );
 };
