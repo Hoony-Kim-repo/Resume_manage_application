2796574c2d8afae36cb43ef7d339866b12b3c138
Hoony-Kim-repo <rlgnsrla512@gmail.com>
2025-10-04 02:15:53 -0400

    Skills tab

--- src/components/InfoTabs/TabContents.jsx
+++ src/components/InfoTabs/TabContents.jsx
@@ -2,6 +2,7 @@ import { Tabs } from "@chakra-ui/react";
 import CustomSwiper from "../common/CustomSwiper";
 import { getExperienceSlides } from "./Contents/Experiences/Experiences";
 import { getProjectSlides } from "./Contents/Projects/ProjectList";
+import Skills from "./Contents/Skills/Skills";
 
 const TabContents = ({ value }) => {
   return (
@@ -12,7 +13,7 @@ const TabContents = ({ value }) => {
       {value === "experiences" && (
         <CustomSwiper>{getExperienceSlides()}</CustomSwiper>
       )}
-      {value === "skills" && <h1>skills</h1>}
+      {value === "skills" && <Skills />}
       {value === "education" && <h1>education</h1>}
     </Tabs.Content>
   );
