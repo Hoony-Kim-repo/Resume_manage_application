6d6cdcbd36210350df38897c8262ae5ded01be86
Hoony-Kim-repo <rlgnsrla512@gmail.com>
2025-10-04 02:17:24 -0400

    Skills tab (#2)

---
+++ src/components/InfoTabs/Contents/Skills/Skills.jsx
@@ -0,0 +1,43 @@
+import { Flex, Image, Text, WrapItem } from "@chakra-ui/react";
+import skills from "./Skills.json";
+
+import styles from "./Skills.module.css";
+
+const Skills = () => {
+  return (
+    <Flex direction="column" className={styles.container}>
+      {skills.map((skill) => (
+        <Flex key={skill.id} align={"center"}>
+          <Text
+            fontSize="2xl"
+            fontWeight="bold"
+            textAlign={"left"}
+            mr={10}
+            flex={1}
+          >
+            {skill.category}
+          </Text>
+
+          <Flex gap={4} wrap={"wrap"} align="center" mt={2} flex={3}>
+            {skill.skills.map((item) => (
+              <WrapItem key={item.id}>
+                <Flex align="center" mr={2} mb={2}>
+                  <Image
+                    boxSize="40px"
+                    objectFit="contain"
+                    src={item.icon}
+                    alt={item.name}
+                    mr={2}
+                  />
+                  <Text fontSize="md">{item.name}</Text>
+                </Flex>
+              </WrapItem>
+            ))}
+          </Flex>
+        </Flex>
+      ))}
+    </Flex>
+  );
+};
+
+export default Skills;
