d4d77d9d9ba55c05da49436a36562eab0ee70da6
Hoony-Kim-repo <rlgnsrla512@gmail.com>
2025-10-07 01:33:03 -0400

    Skills tab (#3)
    * Skills tab
    
    * Education
    
    * Download buttons

--- src/components/InfoTabs/TabContents.jsx
+++ src/components/InfoTabs/TabContents.jsx
@@ -1,5 +1,6 @@
 import { Tabs } from "@chakra-ui/react";
 import CustomSwiper from "../common/CustomSwiper";
+import Education from "./Contents/Education/Education";
 import { getExperienceSlides } from "./Contents/Experiences/Experiences";
 import { getProjectSlides } from "./Contents/Projects/ProjectList";
 import Skills from "./Contents/Skills/Skills";
@@ -14,7 +15,11 @@ const TabContents = ({ value }) => {
         <CustomSwiper>{getExperienceSlides()}</CustomSwiper>
       )}
       {value === "skills" && <Skills />}
+<<<<<<< HEAD
+      {value === "education" && <Education />}
+=======
       {value === "education" && <h1>education</h1>}
+>>>>>>> 6d6cdcbd36210350df38897c8262ae5ded01be86
     </Tabs.Content>
   );
 };
